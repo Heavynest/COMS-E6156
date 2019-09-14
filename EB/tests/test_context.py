36be80644333b7b3e058a5723090e4443542f2cc
Heavynest <1019700136@qq.com>
2019-09-14 04:30:09 -0400

    first commit

---
+++ EB/tests/test_context.py
@@ -0,0 +1,14 @@
+from Context.Context import Context
+import json
+import  os
+
+
+
+def t1():
+
+    ctx = Context.get_default_context()
+
+    db_info = ctx.get_context('db_connect_info')
+    print("DB Connect Info = \n", json.dumps(db_info, indent=2))
+
+t1()
