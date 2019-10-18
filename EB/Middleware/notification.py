d1cda37751ebc275c0a79a408c053fe56bdea5bd
kevin <kevintheduran@gmail.com>
2019-10-18 12:27:46 -0400

    boto3 region added

--- EB/Middleware/notification.py
+++ EB/Middleware/notification.py
@@ -4,7 +4,7 @@ import json
 
 def publish_it(msg):
 
-    client = boto3.client('sns')
+    client = boto3.client('sns', region_name="us-east-2")
     txt_msg = json.dumps(msg)
 
     client.publish(TopicArn="arn:aws:sns:us-east-2:298093020001:E6156CustomerChange",
