3e54bb539dd9ab45db4077a85c94e804396be541
kevin <kevintheduran@gmail.com>
2019-12-16 11:45:06 -0500

    api/profile/<UID> PUT exception msg

--- EB/app.py
+++ EB/app.py
@@ -631,6 +631,7 @@ def profile_uid(uid):
         logger.error(log_msg)
         rsp_status = 500
         rsp_txt = "INTERNAL SERVER ERROR. Please take COMSE6156 -- Cloud Native Applications."
+        rsp_txt = log_msg
         full_rsp = Response(rsp_txt, status=rsp_status, content_type="text/plain")
 
     log_response("/api/profile/<uid>: " + uid, rsp_status, rsp_data, rsp_txt)
